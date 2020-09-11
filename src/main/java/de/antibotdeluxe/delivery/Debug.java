7f5c8dd322ccefd51eeb840a9dc6ad4dea262351
Julian Helpertz <julianhelpertz.dev@gmail.com>
2020-09-11 02:45:58 +0200

    Initial commit

---
+++ src/main/java/de/antibotdeluxe/delivery/Debug.java
@@ -0,0 +1,19 @@
+package de.antibotdeluxe.delivery;
+
+/**
+ * Debugging class for really really simple testing.
+ * For real debugging und testing JUint tests are used.
+ *
+ * @author jhz
+ *
+ * TODO Remove class before releasing the appliction
+ */
+
+public class Debug {
+
+  public static void main(String[] args) {
+    // Method is empty because no code should be pushed. If the tests are not temporary then an individual JUint test should be created
+    // and implemented.
+  }
+
+}
